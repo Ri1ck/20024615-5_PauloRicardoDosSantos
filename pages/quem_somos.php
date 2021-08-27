702bae5322d96b819f691bc36b11035fe0dea011
Ri1ck <89224330+Ri1ck@users.noreply.github.com>
2021-08-26 22:04:50 -0300

    Update quem_somos.php

--- pages/quem_somos.php
+++ pages/quem_somos.php
@@ -3,8 +3,8 @@
 	<br>
 	<img src="./images/Transparente.png" alt="katakuri" class= "katakuri" style="width: 300px;">
 	<h5> A empresa </h5>
-	<p2>Conheça a maior loja de mochi de Santa Catarina. Inspirada no personagem Katakuri de one piece, misturamos a tradicional cultura japonesa com novas receitas modernas, tudo isso para deixar seu mochi mais saboroso. Estamos no mercado a 3 anos e possuimos funcionarios especializados em diversos tipos de mochi. Trabalhamos com encomendas e tele entrega para todo o Brasil.</p2>
+	<p2>Conheça a maior loja de mochi de Santa Catarina. Inspirada no personagem Katakuri de One Piece, misturamos a tradicional cultura japonesa com novas receitas modernas, tudo isso para deixar seu mochi mais saboroso. Estamos no mercado a 3 anos e possuimos funcionarios especializados em diversos tipos de mochi. Trabalhamos com encomendas e tele entrega para todo o Brasil.</p2>
 	<h5>Nosso mochi</h5>
-	<p2>Os ingredientes usados na MOCHIBOX são de alta qualidade, além disso todos os produtos sao analisados e testados. Nossa cozinha possui rigoroza higienização assim como nosso funcionarios. </p2>
+	<p2>Os ingredientes usados na MOCHIBOX são de alta qualidade, além disso todos os produtos são analisados e testados. Nossa cozinha possui rigorosa higienização assim como nosso funcionarios. </p2>
 </div>
 
